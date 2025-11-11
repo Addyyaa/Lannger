6671f0ebca2916f7e4e645c9180c6c9a38385c40
addyya <2698567570@qq.com>
2025-11-11 10:40:06 +0800

    修正 Vite 配置中的基础路径，将其从 '/Langger/' 更改为 '/Lannger/'，并更新 GitHub Actions 工作流中的目标目录设置为 'Lannger'。

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
-  const repoBase = '/Langger/'
+  const repoBase = '/Lannger/'
 
   return {
     plugins: [
