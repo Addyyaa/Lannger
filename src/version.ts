02b77c9301bce3d491fd99824f26fe0720a36f81
addyya <2698567570@qq.com>
2025-11-17 16:12:29 +0800

    Update version to 0.0.18 and enhance documentation with database design proposals. Adjust service worker cache name and improve data consistency checks. Add new review plans table and optimize existing database structures for performance.

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = '0.0.16'
-export const APP_BUILD_TIME = '2025-11-17T08:07:44.358Z'
+export const APP_VERSION = '0.0.18'
+export const APP_BUILD_TIME = '2025-11-17T08:10:57.648Z'
